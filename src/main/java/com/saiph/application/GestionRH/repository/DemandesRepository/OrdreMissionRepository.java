17584d808de87a8928d2019c567619a33aa41ed9
Bassim tabbeb <bassim.tabbeb@ICLOUD.COM>
2024-05-05 08:37:41 +0100

    demande

--- src/main/java/com/saiph/application/GestionRH/repository/DemandesRepository/OrdreMissionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.saiph.application.GestionRH.repository.DemandesRepository;
-
-import com.saiph.application.GestionRH.Domain.entities.Demandes.OrdreMission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface OrdreMissionRepository extends JpaRepository<OrdreMission,Long> {
-}
