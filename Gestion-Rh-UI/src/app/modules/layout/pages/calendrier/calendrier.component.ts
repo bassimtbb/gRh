c15187d42270fce4f2d26ca19ed7b3c1a53545a2
Bassim tabbeb <bassim.tabbeb@ICLOUD.COM>
2024-05-30 02:03:06 +0100

    calendarService

--- Gestion-Rh-UI/src/app/modules/layout/pages/calendrier/calendrier.component.ts
+++ Gestion-Rh-UI/src/app/modules/layout/pages/calendrier/calendrier.component.ts
@@ -27,41 +27,43 @@ export class CalendrierComponent implements OnInit {
     events: [] // Initialize as an empty array
   };
   role: any;
+  filterOptions = {
+    conges: true,
+    formations: true,
+    events: true
+  };
+
+  private allEvents: any[] = [];
 
   constructor(
-    private userService:UserService,
-
+    private userService: UserService,
     private calendarService: CalendarControllerService,
     private congeService: CongeService,
     private formationservice: FormationService,
     private eventservice: EventService,
     private tokenService: TokenService
-  ){}
+  ) { }
 
   ngOnInit(): void {
     this.userId = this.tokenService.Id;
     this.role = this.tokenService.userRole();
 
-
     this.updateCalendarHeight();
-    if(this.role=='RRH')
-    {
+    if (this.role == 'RRH') {
       this.fetchAllCongeValid();
       this.fetchEvents();
       this.fetchFormations();
     }
-    if(this.role=='SUP_H')
-    {
+    if (this.role == 'SUP_H') {
       this.fetchCongeValidByDepartement();
       this.fetchEvents();
       this.fetchFormations();
     }
-    if(this.role=='EMPLOYE') 
-    {    
-    this.fetchEventsByUser();
-    this.fetchFormationsByUser();
-    this.fetchCongeValidByUser();
-  }
+    if (this.role == 'EMPLOYE') {
+      this.fetchEventsByUser();
+      this.fetchFormationsByUser();
+      this.fetchCongeValidByUser();
+    }
   }
 
   @HostListener('window:resize', ['$event'])
@@ -80,8 +82,8 @@ export class CalendrierComponent implements OnInit {
       this.eventservice.findAll3().subscribe(
         (events: EventDto[]) => {
           const formattedEvents = this.formatEvents(events);
-          console.log("events", formattedEvents);
-          this.updateCalendarEvents(formattedEvents);
+          this.allEvents = [...this.allEvents, ...formattedEvents];
+          this.filterEvents();
         },
         error => {
           console.error('Error fetching events:', error);
@@ -89,28 +91,30 @@ export class CalendrierComponent implements OnInit {
       );
     }
   }
+
   fetchEventsByUser(): void {
     if (this.userId) {
-      this.calendarService.getEventsByUserId({userId:this.userId as number})
-      .subscribe(
-        (events: EventDto[]) => {
-          const formattedEvents = this.formatEvents(events);
-          console.log("events", formattedEvents);
-          this.updateCalendarEvents(formattedEvents);
-        },
-        error => {
-          console.error('Error fetching events:', error);
-        }
-      );
+      this.calendarService.getEventsByUserId({ userId: this.userId as number })
+        .subscribe(
+          (events: EventDto[]) => {
+            const formattedEvents = this.formatEvents(events);
+            this.allEvents = [...this.allEvents, ...formattedEvents];
+            this.filterEvents();
+          },
+          error => {
+            console.error('Error fetching events:', error);
+          }
+        );
     }
   }
+
   fetchAllCongeValid(): void {
     if (this.userId) {
-      this.congeService.getCongeByStatut({statut: 'Validee'}).subscribe(
+      this.congeService.getCongeByStatut({ statut: 'Validee' }).subscribe(
         (Conges: CongeDto[]) => {
           const formattedConges = this.formatConges(Conges);
-          console.log("Conges", formattedConges);
-          this.updateCalendarEvents(formattedConges);
+          this.allEvents = [...this.allEvents, ...formattedConges];
+          this.filterEvents();
         },
         error => {
           console.error('Error fetching Conges:', error);
@@ -118,13 +122,14 @@ export class CalendrierComponent implements OnInit {
       );
     }
   }
+
   fetchCongeValidByUser(): void {
     if (this.userId) {
-      this.calendarService.getCongesByUserId({userId:this.userId as number}).subscribe(
+      this.calendarService.getCongesByUserId({ userId: this.userId as number }).subscribe(
         (Conges: CongeDto[]) => {
           const formattedConges = this.formatConges(Conges);
-          console.log("Conges", formattedConges);
-          this.updateCalendarEvents(formattedConges);
+          this.allEvents = [...this.allEvents, ...formattedConges];
+          this.filterEvents();
         },
         error => {
           console.error('Error fetching Conges:', error);
@@ -135,41 +140,29 @@ export class CalendrierComponent implements OnInit {
 
   fetchCongeValidByDepartement(): void {
     if (this.userId) {
-      this.userService.findById({id: this.userId as number  })
-      .subscribe(user => {
-      this.congeService.getCongeBydepartementId({departementId:user.departement?.id as number}).subscribe(
-        (Conges: CongeDto[]) => {
-          const formattedConges = this.formatConges(Conges);
-          console.log("Conges", formattedConges);
-          this.updateCalendarEvents(formattedConges);
-        },
-        error => {
-          console.error('Error fetching Conges:', error);
-        }
-      );
-    })}
+      this.userService.findById({ id: this.userId as number })
+        .subscribe(user => {
+          this.congeService.getCongeBydepartementId({ departementId: user.departement?.id as number }).subscribe(
+            (Conges: CongeDto[]) => {
+              const formattedConges = this.formatConges(Conges);
+              this.allEvents = [...this.allEvents, ...formattedConges];
+              this.filterEvents();
+            },
+            error => {
+              console.error('Error fetching Conges:', error);
+            }
+          );
+        });
+    }
   }
+
   fetchFormations(): void {
     if (this.userId) {
       this.formationservice.findAll2().subscribe(
         (formations: Formation[]) => {
           const formattedFormations = this.formatFormations(formations);
-          console.log("formattedFormations", formattedFormations);
-          this.updateCalendarEvents(formattedFormations);
-        },
-        error => {
-          console.error('Error fetching formations:', error);
-        }
-      );
-    }
-  }
-  fetchFormationsByUser(): void {
-    if (this.userId) {
-      this.calendarService.getFormationsByUserId({userId:this.userId}).subscribe(
-        (formations: Formation[]) => {
-          const formattedFormations = this.formatFormations(formations);
-          console.log("formattedFormations", formattedFormations);
-          this.updateCalendarEvents(formattedFormations);
+          this.allEvents = [...this.allEvents, ...formattedFormations];
+          this.filterEvents();
         },
         error => {
           console.error('Error fetching formations:', error);
@@ -178,26 +171,44 @@ export class CalendrierComponent implements OnInit {
     }
   }
 
- 
+  fetchFormationsByUser(): void {
+    if (this.userId) {
+      this.calendarService.getFormationsByUserId({ userId: this.userId }).subscribe(
+        (formations: Formation[]) => {
+          const formattedFormations = this.formatFormations(formations);
+          this.allEvents = [...this.allEvents, ...formattedFormations];
+          this.filterEvents();
+        },
+        error => {
+          console.error('Error fetching formations:', error);
+        }
+      );
+    }
+  }
+
   addJoursAuDate(debut: Date, jour: number): Date {
     let newDate = new Date(debut);
     newDate.setDate(newDate.getDate() + jour);
     return newDate;
   }
+
   formatEvents(events: EventDto[]): any[] {
-    return events.map(event => ({
-      title: "ÉVÉNEMENT :"+event.titre,
-      start: event.dateD,
-      end: event.dateF,
-      color: '#C00606' // Set the color for events to green
-    }));
+    return events.map(event => {
+      const finDate = event.dateF ? new Date(event.dateF) : new Date();
+      return {
+        title: "ÉVÉNEMENT :" + event.titre,
+        start: event.dateD,
+        end: event.dateF,
+        color: '#C00606' // Set the color for events to green
+      }
+    });
   }
 
   formatConges(conges: CongeDto[]): any[] {
     return conges.map(conge => {
       const debutDate = conge.debut ? new Date(conge.debut) : new Date();
       return {
-        title: conge.utilisateur?.firstname+" " +conge.utilisateur?.lastname ,
+        title: conge.utilisateur?.firstname + " " + conge.utilisateur?.lastname,
         start: debutDate,
         end: this.addJoursAuDate(debutDate, conge.duree!),
         color: '#078CCD' // Set the color for conges to blue
@@ -206,20 +217,35 @@ export class CalendrierComponent implements OnInit {
   }
 
   formatFormations(formations: Formation[]): any[] {
-    return formations.map(formation => ({
-      title: "FORMATION :"+formation.titre,
-      start: formation.dateD,
-      end: formation.dateF,
-      color: '#0E743F' // Set the color for formations to red
-    }));
+    return formations.map(formation => {
+      const finDate = formation.dateF ? new Date(formation.dateF) : new Date();
+      return {
+        title: "FORMATION :" + formation.titre,
+        start: formation.dateD,
+        end: this.addJoursAuDate(finDate, 1),
+        color: '#0E743F' // Set the color for formations to red
+      }
+    });
   }
 
   updateCalendarEvents(newEvents: any[]): void {
-    // Ensure the events property is an array
-    if (!Array.isArray(this.calendarOptions.events)) {
-      this.calendarOptions.events = [];
-    }
-    this.calendarOptions.events = [...this.calendarOptions.events, ...newEvents];
+    this.calendarOptions.events = newEvents;
+  }
+
+  filterEvents(): void {
+    const filteredEvents = this.allEvents.filter(event => {
+      if (this.filterOptions.conges && event.color === '#078CCD') {
+        return true;
+      }
+      if (this.filterOptions.formations && event.color === '#0E743F') {
+        return true;
+      }
+      if (this.filterOptions.events && event.color === '#C00606') {
+        return true;
+      }
+      return false;
+    });
+    this.updateCalendarEvents(filteredEvents);
   }
 
   handleDateClick(arg: DateClickArg): void {
